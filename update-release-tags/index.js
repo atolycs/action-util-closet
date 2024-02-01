4d68c282dcbaa01b0122e2759aa751428f697f2b
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-01 15:48:32 +0900

    Release tag test

--- update-release-tags/index.js
+++ update-release-tags/index.js
@@ -30800,41 +30800,13 @@ async function main() {
 
     const octkit = github.getOctokit(token)
 
-    const major_version = version_tags.split('.')[0]
+    const simple_version_tags = version_tags.replace("ref\/tags\/", "")
 
-    const _alias_sha = await octkit.rest.git.getRef({
-      ...github.context,
-      ref: `tags/${version_tags}`,
-    })
+    const major_version = simple_version_tags.split(".")[0]
 
-    let ref
-    try {
-      ref = await octkit.rest.git.getRef({
-        ...github.context,
-        ref: `tags/${major_version}`,
-      })
-      core.info(`tag ${major_version} already exists.\noverwrite.`)
-    } catch (error) {
-      core.info(`tag ${major_version} does not exist yet.\ncreating.`)
-    }
+    console.log(github.context)
 
-    if (ref) {
-      await octkit.rest.git.updateRef({
-        ...github.context,
-        sha: _alias_sha.object.sha,
-        ref: `tags/${major_version}`,
-        force: true,
-      })
-      core.info(`tag ${version_tags} link to ${major_version}. `)
-    } else {
-      await octkit.rest.git.createRef({
-        ...github.context,
-        sha: _alias_sha.object.sha,
-        ref: `tags/${major_version}`,
-      })
-      core.info(`create tag ${version_tags} link to ${major_version}. `)
-    }
-  } catch (error) {
+   } catch (error) {
     core.setFailed(error)
   }
 }
--- update-release-tags/src/main.js
+++ update-release-tags/src/main.js
@@ -12,31 +12,9 @@ async function main() {
 
     const major_version = simple_version_tags.split(".")[0]
 
-    let ref
-    try {
-        ref = await octkit.rest.git.getRef({
-            ...github.context,
-            ref: `ref/tags/${major_version}`
-        })
-        core.info(`==> ${major_version} already exists. overwrite`)
-    } catch(error) {
-        core.info(`==> ${major_version} does not exsit. creating`)
-    }
+    console.log(github.context)
 
-    if (ref) {
-        await octkit.rest.git.updateRef({
-            ...github.context,
-            ref: `ref/tags/${major_version}`,
-            force: true
-        })
-    } else {
-        await octkit.rest.git.createTag({
-            ...github.context,
-            ref:
-        })
-    }
-
-  } catch (error) {
+   } catch (error) {
     core.setFailed(error)
   }
 }
