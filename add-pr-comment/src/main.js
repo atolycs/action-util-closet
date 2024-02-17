ead06736f50314c952eabf3862aa7a9a6ec61ee5
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-17 23:11:56 +0900

    debug test

--- add-pr-comment/src/main.js
+++ add-pr-comment/src/main.js
@@ -6,14 +6,15 @@ const github = require("@actions/github")
 async function main() {
     try {
         const token = core.getInput("token", { required: true })
-        const pr_number = core.getInput("pr_number", { required: true }) || github.context.pr_number
+        const pr_number = github.context.payload.pull_request.number
         const message = core.getInput("message", { required: true })
 
         const octokit = github.getOctokit(token)
         
-        const result = await octokit.rest.issues.createComment({
+        const result = await octokit.rest.pulls.createReview({
             ...github.context.repo,
-            issue_number: pr_number,
+            pull_number: pr_number,
+            event: "COMMENT",
             body: message
         })
 
