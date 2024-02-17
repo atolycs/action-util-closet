cd05909e7b3d08c30249c2181b7333a03074e6ee
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-17 19:49:06 +0900

    Update add-pr-message reviewers

--- add-pr-comment/index.js
+++ add-pr-comment/index.js
@@ -30799,9 +30799,26 @@ async function main() {
     try {
         const token = core.getInput("token", { required: true })
         const pr_number = core.getInput("pr_number", { required: true }) || github.context.pr_number
+        const reviewers = core.getInput("reviewers", { required: false })
         const message = core.getInput("message", { required: true })
 
         const octokit = github.getOctokit(token)
+
+        const reviewers_count = await octokit.rest.pulls.listReviews({
+            ...github.context.repo,
+            pull_number: pr_number
+        })
+
+        if ( reviewers_count.data.length > 0 ) {
+            core.info("==> Assigned Reviewers")
+        } else {
+            core.info("==> No Reviewers")
+            await octokit.rest.pulls.requestReviewers({
+                ...github.context.repo,
+                pull_number: pr_number,
+                reviewers: reviewers
+            })
+        }
 
         await octokit.rest.pulls.createReviewComment({
             ...github.context.repo,
