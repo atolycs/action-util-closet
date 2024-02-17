ba81c468fe413a07cb38fed1e63959232126e3e5
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-17 19:59:27 +0900

    Fix update

--- add-pr-comment/index.js
+++ add-pr-comment/index.js
@@ -30799,34 +30799,22 @@ async function main() {
     try {
         const token = core.getInput("token", { required: true })
         const pr_number = core.getInput("pr_number", { required: true }) || github.context.pr_number
-        const reviewers = core.getInput("bot-reviewers", { required: false })
         const message = core.getInput("message", { required: true })
 
         const octokit = github.getOctokit(token)
-
-        const reviewers_count = await octokit.rest.pulls.listReviews({
+        
+        await octokit.rest.issues.createComment({
             ...github.context.repo,
-            pull_number: pr_number
-        })
-
-        if ( reviewers_count.data.length > 0 ) {
-            core.info("==> Assigned Reviewers")
-        } else {
-            core.info("==> No Reviewers")
-            await octokit.rest.pulls.requestReviewers({
-                ...github.context.repo,
-                pull_number: pr_number,
-                reviewers: [
-                    reviewers.toString()
-                ]
-            })
-        }
-
-        await octokit.rest.pulls.createReviewComment({
-            ...github.context.repo,
-            pull_number: pr_number,
+            issue_number: pr_number,
             body: message
         })
+
+
+        // await octokit.rest.pulls.createReviewComment({
+        //     ...github.context.repo,
+        //     pull_number: pr_number,
+        //     body: message
+        // })
 
         core.info(`==> PR #${pr_number} to add PR comment`)
     } catch (error) {
